281e1f3568b21ae43a738beef307e198a6605703
Aaron Mayor <aaronkarl.mayor.iics@ust.edu.ph>
2021-10-05 08:41:57 +0800

    added app2.js

--- app2.js
+++ app2.js
@@ -1,13 +1,21 @@
 var fs = require("fs");
+fs.mkdir("./data", function (err){
+    if(err){
+        console.log ("test directory created");
+    } else {
+        console.log ("failed to create test directory");
+    }
+});
+
 var readStream = fs.createReadStream("./info.txt");
-var writeStream = fs.createWriteStream("./info2.txt");
+var writeStream = fs.createWriteStream("./data/info2.txt");
 readStream.pipe(writeStream);
 
-fs.readFile("./info2.txt", "utf-8", (err, data)=> {
+fs.readFile("./data/info2.txt", "utf-8", (err, data)=> {
     if(!err){
         console.log(data);
     }else{
         throw err;
         console("file not found")
     }
-})
+});
